47f9d60a519c880ba0f899f36d627029f8168940
Joshua Hinay <emilianahinay74@gmail.com>
2025-04-15 06:46:24 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from transformers import BartTokenizer, BartForConditionalGeneration
+import torch
+
+app = Flask(__name__)
+CORS(app)
+
+# Load pre-trained BART model and tokenizer
+tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    input_text = data.get('text', '')
+
+    if not input_text:
+        return jsonify({'error': 'No text provided'}), 400
+
+    inputs = tokenizer.encode(input_text, return_tensors='pt', max_length=1024, truncation=True)
+    summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+    return jsonify({'summary': summary})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
