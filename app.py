d2de37d75864618fee39036b87b1cbdb5758bb42
Joshua Hinay <emilianahinay74@gmail.com>
2025-04-15 13:50:34 +0800

    Fix: added missing import for os

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from transformers import BartTokenizer, BartForConditionalGeneration
 import torch
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -26,4 +28,5 @@ def summarize():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
